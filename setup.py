8eedc8819f17175093fd84f252e7081369a0f243
Michael Ricks-Aherne <michael@kubos.com>
2021-05-13 16:10:17 -0500

    initial commit of ci/cd

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'VERSION')) as version_file:
+    version = version_file.read().strip()
+
+with open("README.md", "r") as readme:
+    readme_content = readme.read()
 
 setuptools.setup(
     name="majortom_scripting",
-    version="0.0.1",
+    version=version,
     author="Kubos",
     author_email="open-source@kubos.com",
     description="A package for interacting with Major Tom's Scripting API.",
-    long_description=long_description,
+    long_description=readme_content,
     long_description_content_type="text/markdown",
     url="https://github.com/kubos/majortom_scripting_package",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent"
     ],
     python_requires='>=3.7',
+    keywords='majortom major_tom script kubos major tom satellite',
 )
